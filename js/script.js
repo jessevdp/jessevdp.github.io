35ac103dc32e98ff1ac9d58c3c93ea409a677ef8
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-06-12 13:22:52 +0200

    Fix JavaScript selector
    Make sure the bottom nav doesn’t get the values from the top nav.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $('nav').on('click', '.menu-button', function () {
-  var value = $('nav').attr('data-expanded')
+  var value = $('nav.top').attr('data-expanded')
 
   // Convert string to Boolean
   if (value === 'true') value = true
   if (value === 'false') value = false
   value = !value // Invert value
 
-  $('nav').attr('data-expanded', value)
+  $('nav.top').attr('data-expanded', value)
 })
