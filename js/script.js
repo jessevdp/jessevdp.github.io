99c6d0c07e4999b5e1983f0e760141a9d25fa727
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-06-12 13:54:45 +0200

    Scroll state for top navbar

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ $('nav').on('click', '.menu-button', function () {
 
   $('nav.top').attr('data-expanded', value)
 })
+
+$(window).scroll(function () {
+  var px_scrolled = $(this).scrollTop()
+
+  var $nav= $('nav.top')
+  // Current state -> Boolean
+  var state = $nav.attr('data-scrolled')
+  if (state === 'true') state = true
+  if (state === 'false') state = false
+  // Run the function
+  navScroll($nav, px_scrolled, 10, state)
+})
+
+function navScroll ($element, px_scrolled, breakpoint, current) {
+  if (px_scrolled > breakpoint && current !== true) {
+    $element.attr('data-scrolled', true)
+  } else if (px_scrolled < breakpoint && current !== false) {
+    $element.attr('data-scrolled', false)
+  }
+}
