d3c9afd96d2b3023a2e66abea2c1e9cf2a5da182
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-06-17 00:13:56 +0200

    Basic form validation
    Yay, it works! And it looks pretty good too!

--- assets/js/form.js
+++ assets/js/form.js
@@ -16,3 +16,36 @@ $(document).mouseup(function (e) {
     }
   })
 })
+
+$(document).on('click', '.submit', function () {
+  $('section#form form').valid()
+})
+
+$(document).ready(function (e) {
+  $('section#form form').validate({
+    rules: {
+      subject: 'required',
+      first_name: 'required',
+      last_name: 'required',
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      subject: 'This field is required',
+      first_name: 'This field is required',
+      last_name: 'This field is required',
+      email: {
+        required: 'This field is required',
+        email: 'Please enter a valid email'
+      }
+    },
+    invalidHandler: function (e, validator) {
+      $('html, body').animate({
+        scrollTop: $('section#form').offset().top
+      }, 500)
+      $('#error').addClass('active')
+    }
+  })
+})
